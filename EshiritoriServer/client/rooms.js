7b5be62f3a423ba4fe3771cdcad08554cad73f2a
Hissa Massa <hissamassa7@gmail.com>
2018-02-14 07:20:22 +0900

    roomsを整理

--- EshiritoriServer/client/rooms.js
+++ EshiritoriServer/client/rooms.js
@@ -1,109 +1,84 @@
-//let roomconnection = new Connections.Connection("http://localhost:11451");
-//roomconnection.connect(new Connections.Player("hissa"));
-//roomconnection.setEventListener(Connections.SocketEvent.GetRoomsResponse, data => console.log(data));
-//roomconnection.getRooms();
-//let rConnection = new Connections.Connection2();
-//rConnection.EnterToNewRoom("testRoom", "hissa", data => {
-//    console.log(data);
-//    rConnection.GetRooms(data1 => console.log(data1));
-//});
-function show() {
-    var card = new Components.CardPanel();
-    card.HeaderText = "ルーム一覧";
-    card.Generate($("#body"), "roomList");
-    var newRoomButton = new Components.Button();
-    newRoomButton.Size = Components.ButtonSize.Block;
-    newRoomButton.Style = Components.ButtonStyle.primary;
-    newRoomButton.Text = "新しい部屋を作成";
-    newRoomButton.ClickedEvent = function () {
-        var newRoomModal = new Components.NewRoomModal();
-        newRoomModal.ClickedEnterRoomEvent = function (data) {
-            jumpNewRoom(data.roomName, data.password, data.playerName);
-        };
-        newRoomModal.Generate($("body"));
-        newRoomModal.Show();
-    };
-    newRoomButton.Generate($("#cardpanelroomList"));
-    var table = new Components.RoomList();
-    table.Rooms = rooms;
-    table.ClickedEnterRoomEvent = function (sender) {
-        modal = new Components.RoomModal(sender);
-        modal.Generate($("body"));
-        modal.ClickedEnterRoomEvent = function (data) {
-            if (!data.room.HasPassword) {
-                jump(data.room.Id, data.password, data.playerName);
-            }
-            rconnection.VerifyPassword(data.room.Id, data.password, function (success) {
-                if (success) {
-                    jump(data.room.Id, data.password, data.playerName);
-                }
-                else {
-                    modal.InvalidPassword();
-                }
+var RoomsPage = (function () {
+    function RoomsPage() {
+        var _this = this;
+        this.connection = new Connections.Connection2();
+        this.connection.AddEventListener(Connections.Connection2Event.connect, function () {
+            _this.MakeComponents();
+            _this.AddEventListeners();
+            _this.connection.GetRooms(function (data) {
+                _this.setRooms(data);
+                _this.Update();
             });
-        };
-        modal.Show();
-    };
-    table.Generate($("#cardpanelroomList"));
-    rconnection.AddEventListener(Connections.Connection2Event.RoomsUpdated, function (data) {
-        UpdateRooms(data);
-        table.Rooms = rooms;
-    });
-}
-function jump(roomId, password, playerName) {
-    var query = "?roomId=" + roomId + "&password=" + password + "&playerName=" + playerName;
-    window.location.href = "/index.html" + query;
-}
-function jumpNewRoom(roomName, password, playerName) {
-    var query = "?roomName=" + roomName + "&password=" + password + "&playerName=" + playerName + "&newRoom=true";
-    window.location.href = "/index.html" + query;
-}
-function UpdateRooms(data) {
-    rooms = [];
-    data.forEach(function (value) {
-        var members = [];
-        value.members.forEach(function (player) {
-            members.push(new Components.Player(player.id, player.name));
         });
-        rooms.push(new Components.Room(value.id, value.name, members, value.hasPassword));
-    });
-}
-var rooms = [];
-var rconnection = new Connections.Connection2();
-var modal = null;
-//rconnection.EnterToNewRoom("Testroom", "hissa", "",() => {
-//    rconnection.GetRooms(data => {
-//        data.forEach(value => {
-//            let members: Components.Player[] = [];
-//            value.members.forEach(player => {
-//                members.push(new Components.Player(player.id, player.name));
-//            });
-//            rooms.push(new Components.Room(
-//                value.id,
-//                value.name,
-//                members,
-//                value.hasPassword
-//            ));
-//        });
-//        show();
-//    });
-//});
-rconnection.GetRooms(function (data) {
-    //data.forEach(value => {
-    //    let members: Components.Player[] = [];
-    //    value.members.forEach(player => {
-    //        members.push(new Components.Player(player.id, player.name));
-    //    });
-    //    // constructor(roomId: string, name: string, members: Player[], hasPassword: boolean) {
-    //    rooms.push(new Components.Room(
-    //        value.id,
-    //        value.name,
-    //        members,
-    //        value.hasPassword
-    //    ));
-    //});
-    //show();
-    UpdateRooms(data);
-    show();
-});
+    }
+    RoomsPage.prototype.MakeComponents = function () {
+        this.card = new Components.CardPanel();
+        this.card.HeaderText = "ルーム一覧";
+        this.card.Generate($("#body"), "roomList");
+        this.newRoomButton = new Components.Button();
+        this.newRoomButton.Size = Components.ButtonSize.Block;
+        this.newRoomButton.Style = Components.ButtonStyle.primary;
+        this.newRoomButton.Text = "新しい部屋を作成";
+        this.newRoomButton.Generate(this.card.BodyObject);
+        this.table = new Components.RoomList();
+        this.table.Generate(this.card.BodyObject);
+        this.newRoomModal = new Components.NewRoomModal();
+        this.newRoomModal.Generate($("body"));
+    };
+    RoomsPage.prototype.AddEventListeners = function () {
+        var _this = this;
+        this.connection.AddEventListener(Connections.Connection2Event.RoomsUpdated, function (data) {
+            _this.setRooms(data);
+            _this.Update();
+        });
+        this.newRoomModal.ClickedEnterRoomEvent = function (data) {
+            RoomsPage.jumpNewRoom(data.roomName, data.password, data.playerName);
+        };
+        this.newRoomButton.ClickedEvent = function () {
+            _this.newRoomModal.Show();
+        };
+        this.table.ClickedEnterRoomEvent = function (sender) {
+            _this.EnterRoomModal = new Components.RoomModal(sender);
+            _this.EnterRoomModal.Generate($("body"));
+            _this.EnterRoomModal.Show();
+            _this.EnterRoomModal.ClickedEnterRoomEvent = function (data) {
+                if (!data.room.HasPassword) {
+                    RoomsPage.jump(data.room.Id, data.password, data.playerName);
+                }
+                _this.connection.VerifyPassword(data.room.Id, data.password, function (success) {
+                    if (success) {
+                        RoomsPage.jump(data.room.Id, data.password, data.playerName);
+                    }
+                    else {
+                        _this.EnterRoomModal.InvalidPassword();
+                    }
+                });
+            };
+        };
+    };
+    RoomsPage.prototype.setRooms = function (data) {
+        var _this = this;
+        this.rooms = [];
+        data.forEach(function (value) {
+            var members = [];
+            value.members.forEach(function (player) {
+                members.push(new Components.Player(player.id, player.name));
+            });
+            _this.rooms.push(new Components.Room(value.id, value.name, members, value.hasPassword));
+        });
+    };
+    RoomsPage.prototype.Update = function () {
+        this.table.Rooms = this.rooms;
+    };
+    RoomsPage.jumpNewRoom = function (roomName, password, playerName) {
+        var query = "?roomName=" + roomName + "&password=" + password + "&playerName=" + playerName + "&newRoom=true";
+        window.location.href = "/index.html" + query;
+    };
+    RoomsPage.jump = function (roomId, password, playerName) {
+        var query = "?roomId=" + roomId + "&password=" + password + "&playerName=" + playerName;
+        window.location.href = "/index.html" + query;
+    };
+    return RoomsPage;
+}());
+var roompage = new RoomsPage();
 //# sourceMappingURL=rooms.js.map
--- EshiritoriServer/client/rooms.ts
+++ EshiritoriServer/client/rooms.ts
@@ -1,117 +1,106 @@
-﻿//let roomconnection = new Connections.Connection("http://localhost:11451");
-//roomconnection.connect(new Connections.Player("hissa"));
-//roomconnection.setEventListener(Connections.SocketEvent.GetRoomsResponse, data => console.log(data));
-//roomconnection.getRooms();
-//let rConnection = new Connections.Connection2();
-//rConnection.EnterToNewRoom("testRoom", "hissa", data => {
-//    console.log(data);
-//    rConnection.GetRooms(data1 => console.log(data1));
-//});
+﻿class RoomsPage {
+    // 普通の変数
+    private connection: Connections.Connection2;
+    private rooms: Components.Room[];
+    // コンポーネント
+    private card: Components.CardPanel;
+    private newRoomButton: Components.Button;
+    private table: Components.RoomList;
+    private newRoomModal: Components.NewRoomModal;
+    private EnterRoomModal: Components.RoomModal;
 
-function show() {
-    let card = new Components.CardPanel();
-    card.HeaderText = "ルーム一覧";
-    card.Generate($("#body"), "roomList");
-    let newRoomButton = new Components.Button();
-    newRoomButton.Size = Components.ButtonSize.Block;
-    newRoomButton.Style = Components.ButtonStyle.primary;
-    newRoomButton.Text = "新しい部屋を作成";
-    newRoomButton.ClickedEvent = () => {
-        let newRoomModal = new Components.NewRoomModal();
-        newRoomModal.ClickedEnterRoomEvent = data => {
-            jumpNewRoom(data.roomName, data.password, data.playerName);
-        };
-        newRoomModal.Generate($("body"));
-        newRoomModal.Show();
-    };
-    newRoomButton.Generate($("#cardpanelroomList"));
-    let table = new Components.RoomList();
-    table.Rooms = rooms;
-    table.ClickedEnterRoomEvent = sender => {
-        modal = new Components.RoomModal(sender);
-        modal.Generate($("body"));
-        modal.ClickedEnterRoomEvent = data => {
-            if (!data.room.HasPassword) {
-                jump(data.room.Id, data.password, data.playerName);
-            }
-            rconnection.VerifyPassword(data.room.Id, data.password, success => {
-                if (success) {
-                    jump(data.room.Id, data.password, data.playerName);
-                } else {
-                    modal.InvalidPassword();
-                }
+    constructor() {
+        this.connection = new Connections.Connection2();
+        this.connection.AddEventListener(Connections.Connection2Event.connect, () => {
+            this.MakeComponents();
+            this.AddEventListeners();
+            this.connection.GetRooms(data => {
+                this.setRooms(data);
+                this.Update();
             });
-        };
-        modal.Show();
-    };
-    table.Generate($("#cardpanelroomList"));
-    rconnection.AddEventListener(Connections.Connection2Event.RoomsUpdated, data => {
-        UpdateRooms(data);
-        table.Rooms = rooms;
-    })
-}
-
-function jump(roomId: string, password: string, playerName: string) {
-    let query = `?roomId=${roomId}&password=${password}&playerName=${playerName}`;
-    window.location.href = "/index.html" + query;
-}
-
-function jumpNewRoom(roomName: string, password: string, playerName: string) {
-    let query = `?roomName=${roomName}&password=${password}&playerName=${playerName}&newRoom=true`;
-    window.location.href = "/index.html" + query;
-}
-
-function UpdateRooms(data) {
-    rooms = [];
-    data.forEach(value => {
-        let members: Components.Player[] = [];
-        value.members.forEach(player => {
-            members.push(new Components.Player(player.id, player.name));
         });
-        rooms.push(new Components.Room(
-            value.id,
-            value.name,
-            members,
-            value.hasPassword
-        ));
-    });
+    }
+
+    private MakeComponents() {
+        this.card = new Components.CardPanel();
+        this.card.HeaderText = "ルーム一覧";
+        this.card.Generate($("#body"), "roomList");
+
+        this.newRoomButton = new Components.Button();
+        this.newRoomButton.Size = Components.ButtonSize.Block;
+        this.newRoomButton.Style = Components.ButtonStyle.primary;
+        this.newRoomButton.Text = "新しい部屋を作成";
+        this.newRoomButton.Generate(this.card.BodyObject);
+
+        this.table = new Components.RoomList();
+        this.table.Generate(this.card.BodyObject);
+
+        this.newRoomModal = new Components.NewRoomModal();
+        this.newRoomModal.Generate($("body"));
+    }
+
+    private AddEventListeners() {
+        this.connection.AddEventListener(Connections.Connection2Event.RoomsUpdated, data => {
+            this.setRooms(data);
+            this.Update();
+        });
+
+        this.newRoomModal.ClickedEnterRoomEvent = data => {
+            RoomsPage.jumpNewRoom(data.roomName, data.password, data.playerName);
+        };
+
+        this.newRoomButton.ClickedEvent = () => {
+            this.newRoomModal.Show();
+        };
+
+        this.table.ClickedEnterRoomEvent = sender => {
+            this.EnterRoomModal = new Components.RoomModal(sender);
+            this.EnterRoomModal.Generate($("body"));
+            this.EnterRoomModal.Show();
+            this.EnterRoomModal.ClickedEnterRoomEvent = data => {
+                if (!data.room.HasPassword) {
+                    RoomsPage.jump(data.room.Id, data.password, data.playerName);
+                }
+                this.connection.VerifyPassword(data.room.Id, data.password, success => {
+                    if (success) {
+                        RoomsPage.jump(data.room.Id, data.password, data.playerName);
+                    } else {
+                        this.EnterRoomModal.InvalidPassword();
+                    }
+                });
+            };
+        };
+    }
+
+    private setRooms(data) {
+        this.rooms = [];
+        data.forEach(value => {
+            let members: Components.Player[] = [];
+            value.members.forEach(player => {
+                members.push(new Components.Player(player.id, player.name));
+            });
+            this.rooms.push(new Components.Room(
+                value.id,
+                value.name,
+                members,
+                value.hasPassword
+            ));
+        });
+    }
+
+    private Update() {
+        this.table.Rooms = this.rooms;
+    }
+
+    static jumpNewRoom(roomName: string, password: string, playerName: string) {
+        let query = `?roomName=${roomName}&password=${password}&playerName=${playerName}&newRoom=true`;
+        window.location.href = "/index.html" + query;
+    }
+
+    static jump(roomId: string, password: string, playerName: string) {
+        let query = `?roomId=${roomId}&password=${password}&playerName=${playerName}`;
+        window.location.href = "/index.html" + query;
+    }
 }
 
-let rooms: Components.Room[] = [];
-let rconnection = new Connections.Connection2();
-let modal: Components.RoomModal = null;
-//rconnection.EnterToNewRoom("Testroom", "hissa", "",() => {
-//    rconnection.GetRooms(data => {
-//        data.forEach(value => {
-//            let members: Components.Player[] = [];
-//            value.members.forEach(player => {
-//                members.push(new Components.Player(player.id, player.name));
-//            });
-//            rooms.push(new Components.Room(
-//                value.id,
-//                value.name,
-//                members,
-//                value.hasPassword
-//            ));
-//        });
-//        show();
-//    });
-//});
-rconnection.GetRooms(data => {
-    //data.forEach(value => {
-    //    let members: Components.Player[] = [];
-    //    value.members.forEach(player => {
-    //        members.push(new Components.Player(player.id, player.name));
-    //    });
-    //    // constructor(roomId: string, name: string, members: Player[], hasPassword: boolean) {
-    //    rooms.push(new Components.Room(
-    //        value.id,
-    //        value.name,
-    //        members,
-    //        value.hasPassword
-    //    ));
-    //});
-    //show();
-    UpdateRooms(data);
-    show();
-});
+let roompage = new RoomsPage();
